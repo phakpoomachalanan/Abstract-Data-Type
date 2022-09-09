fe8e5b7b025c323b96a50978e2ca8e38ae75745f
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-09-09 18:47:10 +0700

    PPPPPP--

--- Lab09/lab09-1.c
+++ Lab09/lab09-1.c
@@ -10,6 +10,9 @@ node_t;
 
 typedef node_t bst_t;
 
+bst_t* search_left(bst_t* t);
+bst_t* search_right(bst_t* t);
+bst_t* search(bst_t* t, int data, int mode);
 bst_t* insert(bst_t* t, int data);
 bst_t* delete(bst_t* t, int data);
 int find(bst_t* , int data);
@@ -30,7 +33,7 @@ int main(void) {
                 t = insert(t, data);
                 break;
             case 2:
-                scaanf("%d", &data);
+                scanf("%d", &data);
                 t = delete(t, data);
                 break;
             case 3:
@@ -48,27 +51,131 @@ int main(void) {
     return 0;
 }
 
-bst_t* insert(bst_t* t, int data)
+bst_t* search(bst_t* t, int data, int mode)
 {
+    node_t* node = NULL;
+
+    if (t == NULL || t->data == data)
+    {
+        return t;
+    }
+
+    if (mode == 1)
+    {
+        if ((t->left != NULL && t->left->data == data) || (t->right != NULL && t->right->data == data))
+        {
+            return t;
+        }
+    }
+
+    node = search(t->left, data, mode);
+    if (node != NULL)
+    {
+        return node;
+    }
+    return search(t->right, data, mode);
+
     return NULL;
 }
 
-bst_t* delete(bst_t* t, int data)
+bst_t* search_left(bst_t* t)
 {
-    return NULL;
+    return t->left == NULL ? t : search_left(t->left);
+}
+
+bst_t* search_right(bst_t* t)
+{
+    return t->right == NULL ? t : search_right(t->right);
+}
+
+bst_t* insert(bst_t* t, int data)
+{
+    node_t* node = (node_t*)malloc(sizeof(node_t));
+    node_t* now = t;
+    node_t* pos = t;
+
+    node->data = data;
+
+    if (t == NULL)
+    {
+        return node;
+    }
+
+    while (pos != NULL)
+    {
+        now = pos;
+        if (data > pos->data)
+        {
+            pos = pos->right;
+        }
+        else if (data < pos->data)
+        {
+            pos = pos->left;
+        }
+    }
+
+    if (now->data < data)
+    {
+        now->right = node;
+    }
+    else if (now->data > data)
+    {
+        now->left = node;
+    }
+
+    return t;
+}
+
+bst_t* delete(bst_t* t, int data)
+
+{
+    node_t* node = NULL;
+    if (t == NULL)
+    {
+        return t;
+    }
+
+    if (data > t->data)
+    {
+        t->right = delete(t->right, data);
+    }
+    else if (data < t->data)
+    {
+        t->left = delete(t->left, data);
+    }
+    else
+    {
+        if (t->left == NULL)
+        {
+            node = t->right;
+            free(t);
+            return node;
+        }
+        else if (t->right == NULL)
+        {
+            node = t->left;
+            free(t);
+            return t;
+        }
+        node = search_left(t->right);
+        t->data = node->data;
+        t->right = delete(t->right, node->data);
+    }
+
+    return t;
 }
 
 int find(bst_t* t, int data)
 {
-    return 0;
+    return search(t, data, 0) == NULL ? 0 : 1;
 }
 
 int find_min(bst_t* t)
 {
-    return 0;
+    return t->left == NULL ? t->data : find_min(t->left);
 }
 
 int find_max(bst_t* t)
 {
-    return 0;
+    return t->right == NULL ? t->data : find_max(t->right);
 }
