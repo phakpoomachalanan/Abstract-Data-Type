c45a6c0c064cc81ac51274ac75d1facd8ea19030
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-09-10 14:05:53 +0700

    template for lab09-2

---
+++ Lab09/lab09-2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "week9.h"
+
+#ifndef __avl_tree__
+typedef struct node {
+    int data;
+    int height;
+    struct node *left;
+    struct node *right;
+} node_t;
+
+typedef node_t avl_t;
+#endif
+
+
+
+
+
+
+
+
+int main(void) {
+    avl_t *t = NULL;
+    int n, i;
+    int command, data;
+
+    scanf("%d", &n);
+    for (i=0; i<n; i++) {
+        scanf("%d", &command);
+        switch (command) {
+            case 1:
+                scanf("%d", &data);
+                t = insert(t, data);
+                break;
+            case 2:
+                scanf("%d", &data);
+                t = delete(t, data);
+                break;
+            case 3:
+                print_tree(t);
+                break;
+        }
+    }
+    return 0;
+}
