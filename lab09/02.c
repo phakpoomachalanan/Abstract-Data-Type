b0e04b0a168487b257f1097cf2c7bf500db2410e
KrongPan <89672817+KrongPan@users.noreply.github.com>
2022-09-10 20:49:42 +0700

    lab9-2 (เพิ่มมานิดหน่อย)

--- lab09/02.c
+++ lab09/02.c
@@ -14,9 +14,104 @@ typedef node_t avl_t;
 // ** Note that the print_tree() function
 // has been implemented already and
 // included in the week9.h header
-
+avl_t *find_parent(avl_t *t, int v) {
+  static avl_t *parent = NULL;
+  if (v < t->data) {
+      parent = t;
+      find_parent(t->left, v);
+  } else if (v > t->data) {
+      parent = t;
+      find_parent(t->right, v);
+  } else if (t->data == v) {
+      return parent;
+  } 
+}
+int height(avl_t *t) {
+  static int hl;
+  static int hr;
+  if (t == NULL) {
+    return -1;
+  }
+  hl = height(t->left);
+  hr = height(t->right);
+  return (hl>hr)? hl+1 : hr+1;
+}
+avl_t *right_Ro (avl_t *t,avl_t *a) {
+  avl_t *parent = find_parent(t,a->data);
+  avl_t *b = a->left;
+  a->left = b->right;
+  b->right = a;
+  if (parent != NULL) {
+    if (parent->data > a->data) {
+      parent->left = b;
+    } else {
+      parent->right = b;
+    }
+    return t;
+  } else {
+    return b;
+  }
+}
+avl_t *left_Ro (avl_t *t,avl_t *a) {
+  avl_t *parent = find_parent(t,a->data);
+  avl_t *b = a->right;
+  a->right = b->left;
+  b->left = a;
+  if (parent != NULL) {
+    if (parent->data > a->data) {
+      parent->left = b;
+    } else {
+      parent->right = b;
+    }
+    return t;
+  } else {
+    return b;
+  }
+}
+avl_t *check(avl_t *t, int v) {
+  if ((t->left->height)-(t->right->height) > 1 || (t->left->height)-(t->right->height) < -1) {
+    return t;
+  }
+  if (t->data < v) {
+    check(t->right,v);
+  } else if (t->data > v) {
+    check(t->left,v);
+  } else {
+    return NULL;
+  }
+}
 avl_t *insert(avl_t *t, int v) {
-
+  avl_t *temp = t;
+  avl_t *newnode = (avl_t *)malloc(sizeof(avl_t));
+  newnode->data = v;
+  newnode->left == NULL; newnode->right == NULL;
+  newnode->height = 0;
+  if (t == NULL) {
+    return newnode;
+  } else {
+    while (1) {
+      if (temp > v) {
+        if (temp->left == NULL) {
+          temp->height += 1;
+          temp->left = newnode;
+          return t;
+        } else {
+          temp->height += 1;
+          temp = temp->left;
+        }
+      } else if (temp < v) {
+        if (temp->right == NULL) {
+          temp->height += 1;
+          temp->right = newnode;
+          return t;
+        } else {
+          temp->height += 1;
+          temp = temp->right;
+        }
+      }
+    }
+    avl_t *a = check(t,v);
+  }
 }
 avl_t *delete(avl_t *t, int data) {
 
