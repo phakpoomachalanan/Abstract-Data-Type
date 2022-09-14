d28d7fa9293a3b07c30fb767763967a55acfbe71
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-09-14 11:27:09 +0700

    template for lab10-1.c

---
+++ Lab10/lab10-1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct heap {
+    int *data;
+    int last_index;
+} heap_t;
+
+// Write your code here
+// ...
+
+int main (void) {
+    heap_t *max_heap = NULL;
+    int    m, n, i;
+    int    command, data;
+    int    old_key, new_key;
+
+    scanf("%d %d", &m, &n);
+    max_heap = init_heap(m);
+    for (i=0; i<n; i++) {
+        scanf("%d", &command);
+        switch (command) {
+            case 1:
+                scanf("%d", &data);
+                insert(max_heap, data);
+                break;
+            case 2:
+                delete_max(max_heap);
+                break;
+            case 3:
+                printf("%d\n", find_max(max_heap));
+                break;
+            case 4:
+                scanf("%d %d", &old_key, &new_key);
+                update_key(max_heap, old_key, new_key);
+                break;
+            case 5:
+                bfs(max_heap);
+                break;
+        }
+    }
+    return 0;
+}
