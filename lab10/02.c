c9295e0e9966f953065e9fa44d0740ba63dc898b
KrongPan <89672817+KrongPan@users.noreply.github.com>
2022-09-18 20:57:47 +0700

    10-2 order comple ;-;

--- lab10/02.c
+++ lab10/02.c
@@ -41,60 +41,66 @@ void find_less(heap_t *max_heap, char *word, int data) {
     dataindex/=2;
   }
 }
-void delete_min(heap_t *max_heap, binary_t data) {
+binary_t delete_min1(heap_t *max_heap) {
+  binary_t temp = max_heap->tree[1];
   max_heap->tree[1].word = "\0";
   max_heap->tree[1].data = 0;
   max_heap->tree[1].right = NULL;
   max_heap->tree[1].left = NULL;
-  for (int i = 1;i<max_heap->last_index;i++) {
-    max_heap->tree[i] = max_heap->tree[i+1];
-  }
+  
+  max_heap->tree[1] = max_heap->tree[max_heap->last_index];
+
   max_heap->tree[max_heap->last_index].word = "\0";
   max_heap->tree[max_heap->last_index].data = 0;
   max_heap->tree[max_heap->last_index].right = NULL;
   max_heap->tree[max_heap->last_index].left = NULL;
   max_heap->last_index--;
-  data = max_heap->tree[1];
+  binary_t data = max_heap->tree[1];
   int i = 1;
   
   while ((max_heap->tree[i*2].data != 0 || max_heap->tree[(i*2)+1].data != 0) && i*2 <= max_heap->last_index) {
-    if (max_heap->tree[i*2].data<=max_heap->tree[i].data) {
+    if (max_heap->tree[(i*2)+1].data == 0 && max_heap->tree[(i*2)].data == 0) {
+      break;
+    }
+    if (max_heap->tree[(i*2)+1].data < max_heap->tree[i*2].data && i*2 != max_heap->last_index) {
+      if (max_heap->tree[(i*2)+1].data<max_heap->tree[i].data && max_heap->tree[(i*2)+1].data != 0) {
+      max_heap->tree[i] = max_heap->tree[(i*2)+1];
+      max_heap->tree[(i*2)+1] = data;
+      i = (i*2)+1;
+      } else if (max_heap->tree[(i*2)].data<max_heap->tree[i].data && max_heap->tree[(i*2)].data != 0) {
+        max_heap->tree[i] = max_heap->tree[(i*2)];
+        max_heap->tree[(i*2)] = data;
+        i = (i*2);
+      } else {
+        i++;
+      }
+    } else {
+      if (max_heap->tree[i*2].data<max_heap->tree[i].data && max_heap->tree[(i*2)].data != 0) {
       max_heap->tree[i] = max_heap->tree[i*2];
       max_heap->tree[i*2] = data;
       i = i*2;
-    } else if (max_heap->tree[(i*2)+1].data<=max_heap->tree[i].data) {
+    } else if (max_heap->tree[(i*2)+1].data<max_heap->tree[i].data && max_heap->tree[(i*2)+1].data != 0) {
       max_heap->tree[i] = max_heap->tree[(i*2)+1];
       max_heap->tree[(i*2)+1] = data;
       i = (i*2)+1;
     } else {
       i++;
     }
+    }
   } 
+  return temp;
 }
 void insert(heap_t *max_heap,char *word, int data) {
   max_heap->last_index += 1;
   max_heap->tree[max_heap->last_index].data = data;
   find_less(max_heap, word, data);
 }
-void add_tree(heap_t *max_heap) {
-  max_heap->last_index+=1;
-  max_heap->tree[max_heap->last_index].left = &max_heap->tree[1];
-  max_heap->tree[max_heap->last_index].right = &max_heap->tree[2];
-  max_heap->tree[max_heap->last_index].data = ((max_heap->tree[max_heap->last_index].left->data) + (max_heap->tree[max_heap->last_index].right->data));
+void add_tree(heap_t *max_heap,binary_t left,binary_t right) {
+  max_heap->last_index++;
+  max_heap->tree[max_heap->last_index].left = &left;
+  max_heap->tree[max_heap->last_index].right = &right;
   max_heap->tree[max_heap->last_index].word = "\0";
-  bfs(max_heap);
-  delete_min(max_heap,max_heap->tree[max_heap->last_index]);
-  bfs(max_heap);
-  delete_min(max_heap,max_heap->tree[max_heap->last_index]);
-  bfs(max_heap);
-  int dataindex = max_heap->last_index;
-  binary_t temp = max_heap->tree[dataindex];
-  
-  while (max_heap->tree[dataindex].data <= max_heap->tree[dataindex/2].data && dataindex/2 != 0) {
-    max_heap->tree[dataindex] = max_heap->tree[dataindex/2];
-    max_heap->tree[dataindex/2] = temp;
-    dataindex/=2;
-  }
+  max_heap->tree[max_heap->last_index].data = max_heap->tree[max_heap->last_index].left->data + max_heap->tree[max_heap->last_index].right->data;
 }
 
 void bfs(heap_t *max_heap) {
@@ -110,7 +116,8 @@ int main(void) {
   int  n, i;
   int frequency;
   int old_key, new_key;
-  
+  binary_t right;
+  binary_t left;
   scanf("%d",&n);
   max_heap = init_heap(n);
   for (i=0; i<n; i++) {
@@ -118,8 +125,15 @@ int main(void) {
     insert(max_heap,max_heap->tree[max_heap->last_index+1].word,frequency);
     bfs(max_heap);
   }
-  while (max_heap->tree[2].word != "\0" && max_heap->tree[2].data != 0) {
-    add_tree(max_heap);
+  while (max_heap->tree[2].data != 0) {
+      left = delete_min1(max_heap);
+      printf("de1");
+      bfs(max_heap);
+      right = delete_min1(max_heap);
+      printf("de2");
+      bfs(max_heap);
+      add_tree(max_heap, left, right);
+      printf("add");
     bfs(max_heap);
   }
   printf("end");
--- lab10/tempCodeRunnerFile.c
+++ lab10/tempCodeRunnerFile.c
@@ -1,14 +1,1 @@
-int i = 1;
-  while ((max_heap->data[i*2] != 0 || max_heap->data[(i*2)+1] != 0) && i*2 <= max_heap->last_index) {
-    if (max_heap->data[i*2]>max_heap->data[i]) {
-      max_heap->data[i] = max_heap->data[i*2];
-      max_heap->data[i*2] = data;
-      i = i*2;
-    } else if (max_heap->data[(i*2)+1]>max_heap->data[i]) {
-      max_heap->data[i] = max_heap->data[(i*2)+1];
-      max_heap->data[(i*2)+1] = data;
-      i = (i*2)+1;
-    } else {
-      i++;
-    }
-  }
+max_heap->last_index--;
