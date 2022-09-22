1c2eef70c5a5f257ba3dfae44aef07a174d0978b
nammonman <sirapat69@outlook.com>
2022-09-22 11:09:43 +0700

    where is NULL ?

---
+++ homework/hashTableQuadratic.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TEXTSIZE 21
+
+typedef char* item_t;
+
+typedef struct hash {
+    item_t *table;
+    int size;
+    int hash_key;
+} hash_t;
+
+
+hash_t *init_hashtable(int m,int hash_key) {
+    
+    hash_t *h = (hash_t*)malloc(sizeof(hash_t));
+    h->hash_key = hash_key;
+    h->size = m;
+    h->table = (item_t*)malloc(sizeof(item_t)*m);
+    
+    return h;
+}
+
+unsigned int f(int key ,int n ,char *text) {
+    if (n == 0)
+    {
+        return text[0];
+    }
+    return key*f(key,n-1,text)+text[n];
+}
+
+unsigned int hash(hash_t *hashtable , char *text) {
+    int key = hashtable->hash_key;
+    int n = strlen(text)-1;
+    return(f(key,n,text) % hashtable->size);
+}
+
+int F(int c) {
+    return (c+c*c)/2;
+}
+
+void insert(hash_t *hashtable , char *text) {
+    item_t *i = (item_t*)malloc(sizeof(char)*TEXTSIZE);
+    strcpy(i,text);
+    unsigned int index = hash(hashtable,text);
+    int c = 1;
+    while (hashtable->table[index] != NULL)
+    {
+       index = F(c);
+       c++;
+    }
+    
+    hashtable->table[index] = i;
+}
+
+int search(hash_t *hashtable, char *text) {
+    unsigned int index = hash(hashtable,text);
+    int c = 1;
+    while (hashtable->table[index] != NULL)
+    {
+       if (strcmp(hashtable->table[index],text) == 0)
+       {
+            return index;
+       }
+       
+       index = F(c);
+       c++;
+    }
+    return -1;
+
+}
+
+
+
+
+
+
+int main(void) {
+    hash_t *hashtable = NULL;
+    char *text = NULL;
+    int m,n,i,hash_key;
+    int command;
+
+    scanf("%d %d %d", &m,&n,&hash_key);
+    hashtable = init_hashtable(m,hash_key);
+    text  = (char *)malloc(sizeof(char)*TEXTSIZE);
+
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d %s",&command,text);\
+        switch (command)
+        {
+        case 1:
+            insert(hashtable,text);
+            break;
+        case 2:
+            printf("%d\n",search(hashtable,text));
+            break;
+        }
+    }
+    return 0;
+}
+
---
+++ homework/hashTableSepChain.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TEXTSIZE 21
+
+typedef struct item {
+    char *text;
+    struct item *next;
+} item_t;
+
+typedef struct hash {
+    item_t **table;
+    int size;
+    int hash_key;
+} hash_t;
+
+
+hash_t *init_hashtable(int m,int hash_key) {
+    hash_t *h = (hash_t*)malloc(sizeof(hash_t));
+    h->hash_key = hash_key;
+    h->size = m;
+    h->table = (item_t**)malloc(sizeof(item_t*)*m);
+    
+    return h;
+}
+
+unsigned int f(int key ,int n ,char *text) {
+    if (n == 0)
+    {
+        return text[0];
+    }
+    return key*f(key,n-1,text)+text[n];
+}
+
+unsigned int hash(hash_t *hashtable , char *text) {
+    int key = hashtable->hash_key;
+    int n = strlen(text)-1;
+    return(f(key,n,text) % hashtable->size);
+}
+
+void insert(hash_t *hashtable , char *text) {
+    item_t *i = (item_t*)malloc(sizeof(item_t));
+    i->text = text;
+    i->next = NULL;
+    unsigned int index = hash(hashtable,text); // hash
+    item_t *curr_item = hashtable->table[index];
+    printf("%p",curr_item);
+    while (curr_item != NULL)
+    {
+        curr_item = curr_item->next;
+    }
+    curr_item = i;
+}
+
+int search(hash_t *hashtable, char *text) {
+    unsigned int index = hash(hashtable,text);
+    item_t *curr_item = hashtable->table[index];
+    while (curr_item != NULL)
+    {
+        if (strcmp(curr_item->text , text) == 0)
+        {
+            return index;
+        }
+        curr_item = curr_item->next;
+    }
+    return -1;
+
+}
+
+
+
+int main(void) {
+    hash_t *hashtable = NULL;
+    char *text = NULL;
+    int m,n,i,hash_key;
+    int command;
+
+    scanf("%d %d %d", &m,&n,&hash_key);
+    hashtable = init_hashtable(m,hash_key);
+    text  = (char *)malloc(sizeof(char)*TEXTSIZE);
+
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d %s",&command,text);
+        switch (command)
+        {
+        case 1:
+            insert(hashtable,text);
+            break;
+        case 2:
+            printf("%d\n",search(hashtable,text));
+            break;
+        }
+    }
+    return 0;
+}
+
