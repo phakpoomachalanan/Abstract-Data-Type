44f9903377d3f589c5165cc67cb5045038a1cbd8
KrongPan <89672817+KrongPan@users.noreply.github.com>
2022-09-24 18:11:38 +0700

    11-1 (PPPPPPPPP)

--- lab11/01.c
+++ lab11/01.c
@@ -37,14 +37,11 @@ unsigned int find_hash_value(hash_t *hashtable, char *text) {
 }
 void insert(hash_t *hashtable, char *text) {
   unsigned int hash_value = find_hash_value(hashtable, text);
-  char *newtext;
-  newtext = (char *)malloc(sizeof(char)*TEXTSIZE);
-  printf("[%d]",hash_value);
+  char *newtext = (char *)malloc(sizeof(char)*TEXTSIZE);
   item_t *newitem = (item_t *)malloc(sizeof(item_t));
-  strcpy(text,*newtext);
+  strcpy(newtext,text);
   newitem->text = newtext;
   newitem->next = NULL;
-  printf("%p",hashtable->table[hash_value]);
   if (hashtable->table[hash_value] == NULL) {
     hashtable->table[hash_value] = newitem; 
   } else {
@@ -52,17 +49,32 @@ void insert(hash_t *hashtable, char *text) {
     hashtable->table[hash_value]->next = newitem;
   }
 }
-
+int count_text(item_t *temp, char *text) {
+  if(temp == NULL) {
+    return -1;
+  }
+  int text_countC = 0;
+  int text_count = 0;
+  for(int i=0;i<strlen(text);i++) {
+    text_countC+=(int)text[i];
+  }
+  for(int i=0;i<strlen(temp->text);i++) {
+    text_count+=(int)temp->text[i];
+  }
+  if (text_count == text_countC) {
+    return 1;
+  } else {
+    count_text(temp->next, text);
+  }
+}
 int search(hash_t *hashtable, char *text) {
   unsigned int hash_value = find_hash_value(hashtable, text);
   item_t *temp = hashtable->table[hash_value];
-  while(temp->text != text) {
-    if (temp->next == NULL) {
-      return -1;
-    }
-    temp = temp->next;
+  if (count_text(temp, text) == 1) {
+    return hash_value;
+  } else {
+    return -1;
   }
-  return hash_value;
 }
 
 int main(void) {
@@ -80,15 +92,6 @@ int main(void) {
     switch (command) {
       case 1:
         insert(hashtable, text);
-        for(int i=0;i<m;i++) {
-          printf("%d",i);
-          item_t *temp = hashtable->table[i];
-          while (temp != NULL) {
-            printf("[%s]",temp->text);
-            temp = temp->next;
-          }
-          printf("\n");
-        }
         break;
       case 2:
         printf("%d\n", search(hashtable, text));
