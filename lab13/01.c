57eb2ac84573c38557f9b224fb61af7a6196b3ff
KrongPan <89672817+KrongPan@users.noreply.github.com>
2022-10-10 00:54:28 +0700

    13-1

--- lab13/01.c
+++ lab13/01.c
@@ -5,44 +5,46 @@
 typedef struct data{
   int w;
   int visit;
+  int before;
 } data;
+int count;
 
 int find_shortest_path(int s,int t,int **m,data *d,int n) {
-  printf("{%d}",s);
   d[s].visit = 1;
-  if(d[t].w!=0){
+  //printf("{%d}",s);
+  if (count >= 2*n && d[t].w!=0) {
     return d[t].w;
+  } else if (count >= 2*n){
+    return -1;
   }
-  int i;
-  int min = 99999;
+  
+  int min = 9999999;
   int explor;
-  for (int i=0;i<n;i++) {
-    if (m[s][i]<min && m[s][i]!=0 && d[i].visit == 0) {
-      min = m[s][i];
-      explor = i;
+  for (int j=0;j<n;j++) {
+    if (m[s][j]<min && m[s][j]!=0 && d[j].visit == 0) {
+      min = m[s][j];
+      explor = j;
     }
-    if (m[s][i]!=0) {
-      if (d[i].w == 0 && d[i].visit == 0) {
-        d[i].w += d[s].w+m[s][i];
-      } else if (m[s][i]+d[s].w<d[i].w && d[i].visit == 0) {
-        d[i].w = m[s][i]+d[s].w;
-      } else if (d[i].visit == 0) {
-        d[i].w += d[s].w+m[s][i];
+    if (m[s][j]!=0) {
+      if (d[j].w == 0 && d[j].visit == 0) {
+        d[j].w += d[s].w+m[s][j];
+      } else if (m[s][j]+d[s].w < d[j].w) {
+        d[j].w = m[s][j]+d[s].w;
       }
     }
   }
-
+  count++;
   for (int j=0;j<n;j++) {
-    printf("[%d]",d[j]);
-    Sleep(100);
+    //printf("[%d]",d[j].w);
+    //Sleep(100);
   }
-  printf("\n");
-  i++;
-  if (explor!=t){
+  //printf("\n");
+  if (explor >=n) {
+    find_shortest_path(d[s].before,t,m,d,n);
+    }else {
+    d[explor].before = s;
     find_shortest_path(explor,t,m,d,n);
-  } else if (explor == t){
-    find_shortest_path(explor,t,m,d,n);
-  } 
+  }
 }
 
 int main(void) {
@@ -54,6 +56,7 @@ int main(void) {
     matrix[i] = (int*)malloc(sizeof(int)*n);
     Dijkstra[i].w = 0;
     Dijkstra[i].visit = 0;
+    Dijkstra[i].before = 0;
     for (j=0; j<n; j++) {
       matrix[i][j] = 0;
     }
@@ -64,20 +67,23 @@ int main(void) {
     matrix[u][v] = w;
     matrix[v][u] = w;
   }
+  /*
   for (i=0; i<n; i++) {
     for (j=0; j<n; j++) {
       printf("%3d ",matrix[i][j]);
     }
     printf("\n");
-  }
+  }*/
   int s,t;
   for (j=0; j<p; j++) {
     scanf("%d %d",&s,&t);
     for (int k=0; k<n; k++) {
       Dijkstra[k].w = 0;
       Dijkstra[k].visit = 0;
+      Dijkstra[k].before = 0;
     }
-    printf("%d",find_shortest_path(s,t,matrix,Dijkstra,n));
+    count = 0;
+    printf("%d\n",find_shortest_path(s,t,matrix,Dijkstra,n));
   }
   return 0;
 }
