82ca991a4e32f77d5d6c6308b8601d4d5828b664
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-10-11 21:15:39 +0700

    wth

--- Lab13/lab13-1.c
+++ Lab13/lab13-1.c
@@ -1,183 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pause = 0;
-
-typedef struct node node_t;
-typedef node_t graph_t;
-typedef struct edge edge_t;
-
-struct edge
+typedef struct path
 {
-    int     weight;
-    int     node;
-    edge_t* next;
-};
+    int weight;
+    int visit;
+    int before;
+}
+path_t;
 
-struct node
-{
-    int     vertex;
-    edge_t* edge;
-};
-
-edge_t* init_edge(int weight, int next);
-edge_t* get_weight(graph_t** g, int start, int stop, int now);
-void print(node_t* node);
+void reset_path(path_t* path, int n);
+int get_weight(path_t* path, int** matrix, int start, int stop, int n, int count);
 
 int main(void)
 {
-    int       n, m, p;
-    int       u, v, w;
-    int       i, temp;
-    int       start, stop;
-    graph_t** g;
-    edge_t*   edge;
+    int     n, m, p;
+    int     u, v, w;
+    int     i, j, temp;
+    int     start, stop;
+    int**   matrix = (int**)malloc(sizeof(int*) * n);
+    path_t* path = (path_t*)malloc(sizeof(path_t) * n);
 
     scanf("%d %d %d", &n, &m, &p);
-    g = (graph_t**)malloc(sizeof(node_t*) * n);
 
     for (i=0; i<n; i++)
     {
-        g[i] = (node_t*)malloc(sizeof(node_t));
+        matrix[i] = (int*)malloc(sizeof(int) * n);
+        for (j=0; j<n; j++)
+        {
+            matrix[i][j] = 0;
+        }
     }
     for (i=0; i<n; i++)
     {
         scanf("%d %d %d", &u, &v, &w);
-        if (u > v)
-        {
-            temp = v;
-            v = u;
-            u = temp;
-        }
-        g[u]->vertex = u;
-        edge = g[u]->edge;
-        if (edge == NULL)
-        {
-            g[u]->edge = init_edge(w, v);
-        }
-        else
-        {
-            while (edge->next != NULL)
-            {
-                if (edge->weight > w)
-                {
-                    temp = w;
-                    w = edge->weight;
-                    edge->weight = temp;
-                    temp = v;
-                    v = edge->node;
-                    edge->node = temp;
-                }
-                edge = edge->next;
-            }
-            edge->next = init_edge(w, v);
-        }
-
+        matrix[u][v] = w;
+        matrix[v][u] = w;
     }
     for (i=0; i<p; i++)
     {
-        print(g[i]);
-    }
-    for (i=0; i<p; i++)
-    {
-        //print(g[i]);
         scanf("%d %d", &start, &stop);
-        edge = start < stop ? get_weight(g, start, stop, 0) : get_weight(g, stop, start, 0);
-        pause = 0;
-        if (edge == NULL)
-        {
-            printf("-1\n");
-        }
+        reset_path(path, n);
+        printf("%d\n", get_weight(path, matrix, start, stop, n, 0));
     }
 
     return 0;
 }
 
-edge_t* init_edge(int weight, int node)
+void reset_path(path_t* path, int n)
 {
-    edge_t* edge = (edge_t*)malloc(sizeof(edge_t));
+    int i;
 
-    edge->weight = weight;
-    edge->node = node;
-
-    return edge;
-}
-
-void print(node_t* node)
-{
-    edge_t* next = node->edge;
-
-    while (next != NULL)
+    for (i=0; i<n; i++)
     {
-        printf("%d--%d--%d\n", node->vertex, next->weight, next->node);
-        next = next->next;
+        path[i].before = 0;
+        path[i].visit = 0;
+        path[i].weight = 0;
     }
 }
-/*
-int get_weight(graph_t** g, int start, int stop, int now)
-{
-    static int pause;
-    int i, temp, weight;
-    edge_t* next = g[start]->edge;
 
-    if (g[start]->vertex == stop)
+int get_weight(path_t* path, int** matrix, int start, int stop, int n, int count)
+{
+    int min = 999999999;
+    int i, explore;
+
+    path[start].visit = 1;
+
+    printf("");
+    if (count >= 2*n)
     {
-        return 0;
-    }
-    if (next == NULL)
-    {
-        //pause = -1;
+        if (path[stop].weight != 0)
+        {
+            return path[stop].weight;
+        }
         return -1;
     }
-    printf("%d %d %d %d\n", next->node, stop, now, pause);
-    temp = get_weight(g, next->node, stop, now+next->weight);
-    if (temp == 0)
-    {
-        pause = 1;
-        return now + next->weight;
-    }
-    if (pause == 1)
-    {
-        return temp;
-    }
-    printf("nope %d\n", pause);
-    if (next->next == NULL)
-    {
-        return -1;
-    }
-    return get_weight(g, next->next->node, stop, now+next->weight);
-}
-*/
-edge_t* get_weight(graph_t** g, int start, int stop, int now)
-{
-    edge_t* next = g[start]->edge;
-    edge_t* pos = NULL;
 
-    if (pause == 1)
+    for (i=0; i<n; i++)
     {
-        pause = 0;
-        return next;
+        if (matrix[start][i] != 0)
+        {
+            if (matrix[start][i] < min && path[i].visit == 0)
+            {
+                min = matrix[start][i];
+                explore = i;
+            }
+            if (path[i].weight == 0 && path[i].visit == 0)
+            {
+                path[i].weight += path[start].weight + matrix[start][i];
+            }
+            else if (matrix[start][i]+path[start].weight < path[i].weight)
+            {
+                path[i].weight = path[start].weight + matrix[start][i];
+            }
+        }
     }
-    if (next == NULL)
+    
+    if (explore >= n)
     {
-        return next;
+        return get_weight(path, matrix, path[start].before, stop, n, count+1);
     }
-    if (next->node == stop)
+    else
     {
-        pause = 1;
-        printf("%d\n", now+next->weight);
-        return next;
+        path[explore].before = start;
+        return get_weight(path, matrix, explore, stop, n, count+1);
     }
-
-    pos = get_weight(g, next->node, stop, now+next->weight);
-    if (pos != NULL)
-    {
-        return pos;
-    }
-
-    if (next->next == NULL)
-    {
-        return NULL;
-    }
-    return get_weight(g, next->next->node, stop, now+next->weight);
 }
